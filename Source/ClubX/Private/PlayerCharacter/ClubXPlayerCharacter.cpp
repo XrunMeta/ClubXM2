dee3acf7593ef27bffe6c4b245cfa066ed3f8a93
atthiyya <atthiyya@gmail.com>
2024-08-16 08:26:06 +0700

    Fix jump controller bug

--- Source/ClubX/Private/PlayerCharacter/ClubXPlayerCharacter.cpp
+++ Source/ClubX/Private/PlayerCharacter/ClubXPlayerCharacter.cpp
@@ -113,8 +113,8 @@ void AClubXPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInp
 	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
 	{
 		//Jumping
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+		//EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+		//EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
 
 		//Moving
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AClubXPlayerCharacter::Move);
